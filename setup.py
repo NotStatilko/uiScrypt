af82fb26ab9a662374dde93d06c5954b76785017
NotStatilko <thenonproton@protonmail.com>
2024-04-08 18:32:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name             = 'uiScrypt',
+    version          =  '2.0',
+    packages         =  ['uiScrypt'],
+    license          =  'MIT',
+    description      =  'A simple GUI app for generating Scrypt keys',
+    long_description = open('README.md', encoding='utf-8').read(),
+    author_email     =  'thenonproton@pm.me',
+    url              =  'https://github.com/NotStatilko/uiScrypt',
+    download_url     =  'https://github.com/NotStatilko/uiScrypt/archive/refs/tags/v2.0.tar.gz',
+
+    install_requires = ['pyqt5==5.15.10', 'qrcode==6.1'],
+    long_description_content_type='text/markdown',
+
+    package_data = {
+        'uiScrypt': ['uiScrypt/data'],
+    },
+
+    entry_points = '''
+        [console_scripts]
+        uiScrypt=uiScrypt.uiScrypt:main
+    ''',
+)
