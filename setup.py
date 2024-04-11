e8386dc7a0266653ee554b4d26c751b58f647afe
NotStatilko <thenonproton@protonmail.com>
2024-04-11 09:26:34 +0300

    Use VERSION from version.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
+from ast import literal_eval
+
+with open('uiScrypt/version.py', encoding='utf-8') as f:
+    version = literal_eval(f.read().split('=',1)[1].strip())
 
 setup(
     name             = 'uiScrypt',
-    version          =  '2.0',
+    version          =  version,
     packages         =  ['uiScrypt'],
     license          =  'MIT',
     description      =  'A simple GUI app for generating Scrypt keys',
     long_description = open('README.md', encoding='utf-8').read(),
     author_email     =  'thenonproton@pm.me',
     url              =  'https://github.com/NotStatilko/uiScrypt',
-    download_url     =  'https://github.com/NotStatilko/uiScrypt/archive/refs/tags/v2.0.tar.gz',
+    download_url     =  f'https://github.com/NotStatilko/uiScrypt/archive/refs/tags/v{version}.tar.gz',
 
     install_requires = [
         'pyqt5==5.15.9',
